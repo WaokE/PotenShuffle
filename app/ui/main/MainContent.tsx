a9a5c8d6a6693eca6dd1f2367a80ff702f428af8
Hyungjun <dlgudwns1207@gmail.com>
2023-12-11 03:14:38 +0900

    Feat: Add Filer component

--- app/ui/main/MainContent.tsx
+++ app/ui/main/MainContent.tsx
@@ -1,10 +1,12 @@
 import TempCard from "./IntroCard";
+import Filter from "./Filter/Filter";
 import CardWrapper from "./CardWrapper";
 
 export default function MainContent() {
     return (
         <div className="flex flex-col gap-8 w-2/3 h-full">
             <TempCard />
+            <Filter />
             <CardWrapper />
         </div>
     );
