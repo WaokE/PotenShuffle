0a914c7dc7a4669f9a2808a65c35f854d9f94680
Hyungjun <dlgudwns1207@gmail.com>
2023-12-10 18:29:39 +0900

    Feat: Adjust card CSS

--- app/ui/home/Card.tsx
+++ app/ui/home/Card.tsx
@@ -1,6 +1,6 @@
 export default function Card() {
     return (
-        <div className="w-full h-1/4-screen border-solid border-2 border-sky-500">
+        <div className="w-full h-1/4-screen bg-gray-300 rounded-lg">
             <p>Name</p>
             <p>Email</p>
             <p>Keyword</p>
--- app/ui/home/TempCard.tsx
+++ app/ui/home/TempCard.tsx
@@ -1,6 +1,6 @@
 export default function TempCard() {
     return (
-        <div className="bg-gray-400 h-1/2-screen rounded-lg">
+        <div className="bg-gray-300 h-1/2-screen rounded-lg">
             <button>자기 소개 작성하러 가기</button>
         </div>
     );
