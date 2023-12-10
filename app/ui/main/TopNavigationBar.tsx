1449493846379c7a7ac8db9171d7c70a128400ad
WaokE <dlgudwns1207@gmail.com>
2023-12-11 03:52:00 +0900

    Style: Adjust TopNavigation text position

--- app/ui/main/TopNavigationBar.tsx
+++ app/ui/main/TopNavigationBar.tsx
@@ -1,8 +1,8 @@
 export default function TopNavigationBar() {
     return (
         <div className="flex justify-between items-center w-full h-1/20-screen p-4 border-solid border-2 border-black-400">
-            <span>팀 매칭 제목</span>
-            <span>로그인</span>
+            <span className="ml-[17%]">팀 매칭 제목</span>
+            <span className="mr-[17%]">로그인</span>
         </div>
     );
 }
