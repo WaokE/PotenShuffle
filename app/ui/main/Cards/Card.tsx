032a656739852c88a5062ad8864acea6ee5757e7
WaokE <dlgudwns1207@gmail.com>
2023-12-11 03:48:41 +0900

    Feat: Cards wireframe

--- app/ui/main/Cards/Card.tsx
+++ app/ui/main/Cards/Card.tsx
@@ -1,9 +1,44 @@
-export default function Card() {
+import Image from "next/image";
+
+type CardData = {
+    name: string;
+    role: string;
+    description: string;
+    keywords: string[];
+};
+
+const Card: React.FC<CardData> = ({ name, role, description, keywords }) => {
     return (
-        <div className="w-full h-1/4-screen bg-gray-300 rounded-lg">
-            <p>Name</p>
-            <p>Email</p>
-            <p>Keyword</p>
+        <div className="flex flex-col p-4 gap-4 w-full h-1/2-screen border shadow-lg rounded-lg">
+            <div className="flex justify-between items-center">
+                <p className="text-2xl font-[700]">{name}</p>
+                <Image src="/icon.png" alt="icon" width="20" height="20" />
+            </div>
+            <p className="font-[700]">{role}</p>
+            {role === "designer" ? (
+                <Image src="/_Type=3.png" alt="icon" width="100" height="20" />
+            ) : null}
+            {role === "FE developer" ? (
+                <Image src="/_Type=2.png" alt="icon" width="100" height="20" />
+            ) : null}
+            {role === "BE developer" ? (
+                <Image src="/_Type=4.png" alt="icon" width="100" height="20" />
+            ) : null}
+            {role === "product manager" ? (
+                <Image src="/_Type=1.png" alt="icon" width="100" height="20" />
+            ) : null}
+            <p>{description}</p>
+            <div className="flex flex-wrap gap-2">
+                {keywords.map((keyword) => (
+                    <div className="bg-gray-200 rounded-full p-2">{keyword}</div>
+                ))}
+            </div>
         </div>
     );
+};
+
+{
+    /* <div className="w-full h-1/3-screen border shadow-lg rounded-lg"> */
 }
+
+export default Card;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
             height: {
                 "2/3-screen": "66.666667vh",
                 "1/2-screen": "50vh",
+                "1/3-screen": "33.333333vh",
                 "1/4-screen": "25vh",
                 "1/10-screen": "10vh",
                 "1/20-screen": "5vh",
