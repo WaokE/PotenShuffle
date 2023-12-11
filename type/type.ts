f6aae641cafc6daa4995bfaf55765a1f4e64de85
Hyungjun <dlgudwns1207@gmail.com>
2023-12-11 18:11:31 +0900

    Feat: Fetch filtered user info

---
+++ type/type.ts
@@ -0,0 +1,14 @@
+export type fetchUsersFilter = {
+    employmentStatus: "NONE" | "CURRENTLY_WORKING" | "LOOKING_FOR_JOB" | "PREPARING_FOR_EMPLOYMENT";
+    teampBuildingStatus: "WAITING" | "IN_PROGRESS" | "COMPLETED";
+    occupation:
+        | "NONE"
+        | "PRODUCT_MANAGER"
+        | "DESIGNER"
+        | "SERVER_ENGINEER"
+        | "WEB_ENGINEER"
+        | "AOS ENGINEER"
+        | "IOS_ENGINEER";
+    page: number;
+    size: number;
+};
