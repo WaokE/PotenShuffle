4f7799217355e59cac90ef1af5108ade92fe3c45
Hyungjun <dlgudwns1207@gmail.com>
2023-12-13 04:47:42 +0900

    Style: Change MainBaner title

--- app/ui/main/MainBanner.tsx
+++ app/ui/main/MainBanner.tsx
@@ -3,18 +3,15 @@ import Link from "next/link";
 export default function MainBanner() {
     return (
         <div
-            className="flex flex-col gap-4 h-1/2-screen rounded-lg p-8"
+            className="flex flex-col gap-4 h-1/2-screen rounded-lg p-16"
             style={{
                 backgroundImage: "url('/Gradient.png')",
                 backgroundSize: "cover",
                 backgroundPosition: "center",
             }}
         >
-            <p className="text-[90px] text-white font-[700]">
-                FIND YOUR
-                <br />
-                TEAMMATE
-            </p>
+            <p className="text-[70px] text-white font-[200]">포텐셔플</p>
+            <p className="text-[30px] text-white font-[200]">Shuffle your potential</p>
             <div className="flex justify-center items-center h-1/6">
                 <Link
                     href={"./main/chatbot"}
