aaba51afd2e9d8bff0fef1514cacb896dacf24bf
Hyungjun <dlgudwns1207@gmail.com>
2023-12-12 23:09:19 +0900

    Debug: Routing bug

--- app/ui/main/TopNavigationBar.tsx
+++ app/ui/main/TopNavigationBar.tsx
@@ -4,7 +4,7 @@ export default function TopNavigationBar() {
     return (
         <div className="flex justify-between items-center w-full h-1/20-screen p-4 border-solid border-2 border-black-400">
             <span className="ml-[17%]">팀 매칭 제목</span>
-            <Link href={"./login"} className="mr-[17%]">
+            <Link href="/login" className="mr-[17%]">
                 로그인
             </Link>
         </div>
