ec114291578db6ccff2ad109206c4c684bf67564
Hyungjun <dlgudwns1207@gmail.com>
2023-12-12 20:00:09 +0900

    Feat: Adjust Chip component style

--- app/ui/main/Chip.tsx
+++ app/ui/main/Chip.tsx
@@ -11,11 +11,11 @@ type ChipProps = {
 const Chip: React.FC<ChipProps> = ({ label, onClick, bgColor, textColor, borderColor }) => {
     return (
         <div
-            className={`flex justify-center items-center border rounded-full px-2 py-1 select-none`}
+            className={`flex justify-center items-center border rounded-full px-4 py-2 select-none cursor-pointer`}
             style={{ backgroundColor: bgColor, color: textColor, borderColor: borderColor }}
             onClick={onClick}
         >
-            <p className="text-sm">{label}</p>
+            <p className="text-sm font-[700]">{label}</p>
         </div>
     );
 };
