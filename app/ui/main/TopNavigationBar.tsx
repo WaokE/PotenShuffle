e3ca3f070048ddf91454b0b7ab55ba709bc38657
Hyungjun <dlgudwns1207@gmail.com>
2023-12-13 06:29:39 +0900

    Style: TopNavigationBar component

--- app/ui/main/TopNavigationBar.tsx
+++ app/ui/main/TopNavigationBar.tsx
@@ -3,7 +3,11 @@ import Link from "next/link";
 export default function TopNavigationBar() {
     return (
         <div className="flex justify-between items-center w-full h-1/20-screen p-4 border-solid border-2 border-black-400">
-            <span className="ml-[17%]">팀 매칭 제목</span>
+            <div className="flex justify-center items-center gap-8 ml-[17%]">
+                <span className="font-[700] text-xl">포텐셔플</span>
+                <span className="font-[700] text-sm">자기 소개 보기</span>
+                <span className="font-[700] text-sm">빌딩 중인 그룹 보기</span>
+            </div>
             <Link href="/login" className="mr-[17%]">
                 로그인
             </Link>
