e7182e96cf9bffe2a846735d4fafe5c6312d4e0f
Hyungjun <dlgudwns1207@gmail.com>
2023-12-12 21:52:02 +0900

    Feat: Show selected filter dynamically

--- app/ui/main/Filter/Filter.tsx
+++ app/ui/main/Filter/Filter.tsx
@@ -3,18 +3,34 @@ import Chip from "../Chip";
 import { useFilterStore } from "@/store/filterStore";
 
 export default function Filter() {
-    const { isModalOpen, toggleIsModalOpen } = useFilterStore();
+    const { isModalOpen, toggleIsModalOpen, selectedFilter } = useFilterStore();
     return (
         <div className="flex gap-2">
-            <Chip
-                label="필터"
-                onClick={() => {
-                    toggleIsModalOpen();
-                }}
-                bgColor={isModalOpen ? "#F6EFFF" : "#FFFFFF"}
-                textColor={isModalOpen ? "#7A34F2" : "#121213"}
-                borderColor={isModalOpen ? "#7A34F2" : "#E0E0E0"}
-            />
+            {selectedFilter.map((item, index) => {
+                return (
+                    <Chip
+                        key={index}
+                        label={item}
+                        onClick={() => {
+                            toggleIsModalOpen();
+                        }}
+                        bgColor={"#F6EFFF"}
+                        textColor={"#7A34F2"}
+                        borderColor={"#7A34F2"}
+                    />
+                );
+            })}
+            <div className="ml-auto">
+                <Chip
+                    label="필터"
+                    onClick={() => {
+                        toggleIsModalOpen();
+                    }}
+                    bgColor={isModalOpen ? "#F6EFFF" : "#FFFFFF"}
+                    textColor={isModalOpen ? "#7A34F2" : "#121213"}
+                    borderColor={isModalOpen ? "#7A34F2" : "#E0E0E0"}
+                />
+            </div>
         </div>
     );
 }
