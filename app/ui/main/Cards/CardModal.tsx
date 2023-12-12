83c03556c101a2f8bd848b494b39cf4942a0db1b
Hyungjun <dlgudwns1207@gmail.com>
2023-12-12 19:59:33 +0900

    Feat: Implement modal functions

--- app/ui/main/Cards/CardModal.tsx
+++ app/ui/main/Cards/CardModal.tsx
@@ -6,8 +6,16 @@ import { useCardModalStore } from "@/store/cardModalStore";
 const CardModal = () => {
     const { setIsModalOpen, selectedCardData } = useCardModalStore();
     return (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-            <div className="relative bg-white p-4 rounded-[20px] shadow-md">
+        <div
+            className="fixed inset-0 flex items-center justify-center z-50"
+            onClick={() => {
+                setIsModalOpen(false);
+            }}
+        >
+            <div
+                className="relative bg-white p-4 rounded-[20px] shadow-md"
+                onClick={(e) => e.stopPropagation()}
+            >
                 <button
                     className="absolute top-4 right-4 text-lg text-gray-400"
                     onClick={() => {
@@ -16,9 +24,14 @@ const CardModal = () => {
                 >
                     &#x2715;
                 </button>
-                <div className="flex p-10 gap-8">
+                <div className="flex p-5 gap-8">
                     <Card cardData={selectedCardData} />
                     <TextCard text={selectedCardData.preferredTeamMember} />
+                </div>
+                <div className="flex justify-center w-auto">
+                    <button className="bg-[#7A34F2] rounded-[20px] text-white font-[700] p-4 w-1/5">
+                        제안 보내기
+                    </button>
                 </div>
             </div>
         </div>
