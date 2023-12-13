c58399f6ebc19cbc80a2a0a0e2aaac0b91837c5d
Hyungjun <dlgudwns1207@gmail.com>
2023-12-13 17:35:56 +0900

    Style: Adjust border

--- app/ui/main/TopNavigationBar.tsx
+++ app/ui/main/TopNavigationBar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function TopNavigationBar() {
     return (
-        <div className="flex justify-between items-center w-full h-7.5%-screen p-4 border-solid border-2 border-black-400">
+        <div className="flex justify-between items-center w-full h-7.5%-screen p-4 border-solid border-b-2 border-black-400">
             <div className="flex justify-center items-center gap-8 ml-[17%]">
                 <span className="font-[700] text-xl">포텐셔플</span>
                 <span className="font-[700] text-sm">자기 소개 보기</span>
