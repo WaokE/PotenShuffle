46fe8a2c8a76e1ffdbac896ac36f99fb6d08365e
Hyungjun <dlgudwns1207@gmail.com>
2023-12-14 03:43:03 +0900

    Feat: Add temp route on chat send button

--- app/ui/chatbot/ChatFooter.tsx
+++ app/ui/chatbot/ChatFooter.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function ChatFooter() {
     return (
         <div className="flex border-2">
             <input className="flex-grow p-2" placeholder="답변을 입력해주세요" />
-            <button className="p-2">전송</button>
+            <Link href="/main/introduce">
+                <button className="p-2">전송</button>
+            </Link>
         </div>
     );
 }
