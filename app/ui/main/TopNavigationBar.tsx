217ece7b0992fc76dc983558601958f89f506bea
Hyungjun <dlgudwns1207@gmail.com>
2023-12-14 03:59:10 +0900

    Feat: Add Link on TopNavigationBar

--- app/ui/main/TopNavigationBar.tsx
+++ app/ui/main/TopNavigationBar.tsx
@@ -4,7 +4,9 @@ export default function TopNavigationBar() {
     return (
         <div className="flex justify-between items-center w-full h-7.5%-screen p-4 border-solid border-b-2 border-black-400">
             <div className="flex justify-center items-center gap-8 ml-[17%]">
-                <span className="font-bold text-xl">포텐셔플</span>
+                <Link href="/main">
+                    <span className="font-bold text-xl">포텐셔플</span>
+                </Link>
                 <span
                     className="font-bold text-sm underline text-purple-500"
                     style={{ textDecorationThickness: "2px", textUnderlineOffset: "10px" }}
