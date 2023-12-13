c0d28a6591e0c5dc8c575424d842ccb799c8eba0
Hyungjun <dlgudwns1207@gmail.com>
2023-12-14 03:55:16 +0900

    Feat: Add static style on TopNavigationBar

--- app/ui/main/TopNavigationBar.tsx
+++ app/ui/main/TopNavigationBar.tsx
@@ -4,9 +4,14 @@ export default function TopNavigationBar() {
     return (
         <div className="flex justify-between items-center w-full h-7.5%-screen p-4 border-solid border-b-2 border-black-400">
             <div className="flex justify-center items-center gap-8 ml-[17%]">
-                <span className="font-[700] text-xl">포텐셔플</span>
-                <span className="font-[700] text-sm">자기 소개 보기</span>
-                <span className="font-[700] text-sm">빌딩 중인 그룹 보기</span>
+                <span className="font-bold text-xl">포텐셔플</span>
+                <span
+                    className="font-bold text-sm underline text-purple-500"
+                    style={{ textDecorationThickness: "2px", textUnderlineOffset: "10px" }}
+                >
+                    자기 소개 보기
+                </span>
+                <span className="font-bold text-sm">빌딩 중인 그룹 보기</span>
             </div>
             <Link href="/login" className="mr-[17%]">
                 로그인
