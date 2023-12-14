7ec45d5e08e0acd829da266cdb7e29ba0ca11286
Hyungjun <dlgudwns1207@gmail.com>
2023-12-14 12:38:38 +0900

    Style: Lottie animation size

--- app/main/sending_offer/page.tsx
+++ app/main/sending_offer/page.tsx
@@ -4,8 +4,8 @@ import SendingOffer from "@/app/ui/SendingOffer";
 
 export default function sendingOfferPage() {
     return (
-        <>
+        <div className="w-1/3">
             <SendingOffer />
-        </>
+        </div>
     );
 }
