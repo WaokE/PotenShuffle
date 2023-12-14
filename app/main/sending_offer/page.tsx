8cabb1334f3c1cfa57be743a20e7d52e4fccab5f
Hyungjun <dlgudwns1207@gmail.com>
2023-12-14 12:37:14 +0900

    Feat: Add lottie animation sample

---
+++ app/main/sending_offer/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import SendingOffer from "@/app/ui/SendingOffer";
+
+export default function sendingOfferPage() {
+    return (
+        <>
+            <SendingOffer />
+        </>
+    );
+}
---
+++ app/ui/SendingOffer.tsx
@@ -0,0 +1,8 @@
+import animation from "./animation.json";
+import Lottie from "lottie-react";
+
+const sendingOffer = () => {
+    return <Lottie animationData={animation} loop={true} />;
+};
+
+export default sendingOffer;
