5828daf1591b422cfd771053418080cadd0de77e
Hyungjun <dlgudwns1207@gmail.com>
2023-12-16 00:35:15 +0900

    Feat: Protect route before login

--- app/main/chatbot/page.tsx
+++ app/main/chatbot/page.tsx
@@ -1,6 +1,24 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 import ChatWrapper from "@/app/ui/chatbot/ChatWrapper";
 
+import { useCurrentUserStore } from "@/store/currentUserStore";
+
+import { validLogin } from "@/app/lib/vaildLogin";
+
 export default function ChatbotPage() {
+    const expiredData = useCurrentUserStore((state) => state.user.tokenExpire);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!validLogin(expiredData)) router.push("/login");
+    }, []);
+
+    if (!validLogin(expiredData)) return null;
+
     return (
         <div className="flex justify-center">
             <ChatWrapper />
--- app/main/introduce/page.tsx
+++ app/main/introduce/page.tsx
@@ -1,8 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 import IntroduceMessage from "@/app/ui/main/introduce/IntroduceMessage";
 import IntroduceFormContainer from "@/app/ui/main/introduce/IntroduceFormContainer";
 import ButtonContainer from "@/app/ui/main/introduce/ButtonContainer";
 
-export default function IntroducePage() {
+import { useCurrentUserStore } from "@/store/currentUserStore";
+
+import { validLogin } from "@/app/lib/vaildLogin";
+
+const IntroducePage = () => {
+    const expiredData = useCurrentUserStore((state) => state.user.tokenExpire);
+    const router = useRouter();
+    useEffect(() => {
+        if (!validLogin(expiredData)) router.push("/login");
+    }, []);
+
+    if (!validLogin(expiredData)) return null;
+
     return (
         <div className="rounded-[20px] flex flex-col gap-8 p-8 mb-8 mt-4 w-2/3 h-auto bg-[#FBFBFB]">
             <IntroduceMessage />
@@ -10,4 +27,6 @@ export default function IntroducePage() {
             <ButtonContainer />
         </div>
     );
-}
+};
+
+export default IntroducePage;
