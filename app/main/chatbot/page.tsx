0542445cef5b6de59a5d7170578aa92ac3ebfe04
Hyungjun <dlgudwns1207@gmail.com>
2023-12-16 01:20:45 +0900

    Debug: Routing error caused by Zustand

--- app/main/chatbot/page.tsx
+++ app/main/chatbot/page.tsx
@@ -5,18 +5,26 @@ import { useRouter } from "next/navigation";
 
 import ChatWrapper from "@/app/ui/chatbot/ChatWrapper";
 
+import useStore from "@/store/useStore";
+import { useCurrentUserStore } from "@/store/currentUserStore";
+
 import { validLogin } from "@/app/lib/vaildLogin";
 
 export default function ChatbotPage() {
-    const userData = localStorage.getItem("user-storage");
-    const expireDate = JSON.parse(userData!).state.user.tokenExpire;
+    // expireDate: string | undefined (for evade hydration error)
+    const expireDate = useStore(useCurrentUserStore, (state) => state.user.tokenExpire);
 
     const router = useRouter();
 
     useEffect(() => {
-        if (!validLogin(expireDate!)) router.push("/login");
-    }, []);
+        // expireDate가 undefined일 때는 라우팅 검사 스킵
+        if (expireDate === undefined) return;
+        if (!expireDate || !validLogin(expireDate)) {
+            router.push("/login");
+        }
+    }, [expireDate]);
 
+    // 렌더링 중지 (undefined일 때 | 로그인 유효기간이 지났을 때)
     if (!validLogin(expireDate!)) return null;
 
     return (
--- app/main/introduce/page.tsx
+++ app/main/introduce/page.tsx
@@ -7,18 +7,26 @@ import IntroduceMessage from "@/app/ui/main/introduce/IntroduceMessage";
 import IntroduceFormContainer from "@/app/ui/main/introduce/IntroduceFormContainer";
 import ButtonContainer from "@/app/ui/main/introduce/ButtonContainer";
 
+import useStore from "@/store/useStore";
+import { useCurrentUserStore } from "@/store/currentUserStore";
+
 import { validLogin } from "@/app/lib/vaildLogin";
 
 const IntroducePage = () => {
-    const userData = localStorage.getItem("user-storage");
-    const expireDate = JSON.parse(userData!).state.user.tokenExpire;
+    // expireDate: string | undefined (for evade hydration error)
+    const expireDate = useStore(useCurrentUserStore, (state) => state.user.tokenExpire);
 
     const router = useRouter();
     useEffect(() => {
-        if (!validLogin(expireDate)) router.push("/login");
-    }, []);
+        // expireDate가 undefined일 때는 라우팅 검사 스킵
+        if (expireDate === undefined) return;
+        if (!expireDate || !validLogin(expireDate)) {
+            router.push("/login");
+        }
+    }, [expireDate]);
 
-    if (!validLogin(expireDate)) return null;
+    // 렌더링 중지 (undefined일 때 | 로그인 유효기간이 지났을 때)
+    if (!validLogin(expireDate!)) return null;
 
     return (
         <div className="rounded-[20px] flex flex-col gap-8 p-8 mb-8 mt-4 w-2/3 h-auto bg-[#FBFBFB]">
