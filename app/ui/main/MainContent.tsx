1c2d66617f319f38cd2edd3d06d6918de2d6283b
Hyungjun <dlgudwns1207@gmail.com>
2023-12-17 02:34:21 +0900

    Feat: Add Temp Teams component

--- app/ui/main/MainContent.tsx
+++ app/ui/main/MainContent.tsx
@@ -6,20 +6,28 @@ import FilterModal from "./Filter/FilterModal";
 import CardWrapper from "./Cards/CardWrapper";
 import CurrentUserDashboardWrapper from "./Dashboard/CurrentUserDashboardWrapper";
 import MidNavigationBar from "./MidNavigationBar";
+import Teams from "./Teams/Teams";
 
 // State stores
 import { useFilterStore } from "@/store/filterStore";
+import { useMidNavigationStore } from "@/store/midNavigationStore";
 
 export default function MainContent() {
     const { isModalOpen } = useFilterStore();
+    const { selectedSection } = useMidNavigationStore();
 
     return (
         <div className="flex flex-col gap-8 w-2/3 h-full">
             <CurrentUserDashboardWrapper />
             <MidNavigationBar />
-            <Filter />
             {isModalOpen && <FilterModal />}
-            <CardWrapper />
+            {selectedSection === "personalCards" && (
+                <>
+                    <Filter />
+                    <CardWrapper />
+                </>
+            )}
+            {selectedSection === "buildingTeams" && <Teams />}
         </div>
     );
 }
