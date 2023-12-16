094fd294f4b99663b549886c334a6c507210cada
Hyungjun <dlgudwns1207@gmail.com>
2023-12-16 23:23:02 +0900

    Style: Adjust chat style, Add chatbot profile image

--- app/ui/chatbot/ChatBody.tsx
+++ app/ui/chatbot/ChatBody.tsx
@@ -13,10 +13,8 @@ export default function ChatBody() {
                             className="flex gap-4 justify-start text-white"
                             key={`message-${index}`}
                         >
-                            <div className="w-12 h-12 rounded-full bg-gray-400 flex justify-center items-center">
-                                챗봇
-                            </div>
-                            <div className="bg-[#7A34F2] p-3 rounded-[15px] max-w-[50%] break-words">
+                            <div className="w-[60px] h-[60px] rounded-full bg-[url('/chatBotProfile.svg')]" />
+                            <div className="bg-[#FAF6FF] p-3 rounded-[15px] text-[#46148D] max-w-[50%] break-words shadow-md">
                                 {data.content}
                             </div>
                         </div>
@@ -26,7 +24,7 @@ export default function ChatBody() {
                 else {
                     return (
                         <div className="flex justify-end" key={`message-${index}`}>
-                            <div className="bg-[#F6EFFF] p-3 rounded-[15px] max-w-[50%] break-words">
+                            <div className="bg-[#FFFFFF] p-3 rounded-[15px] max-w-[50%] break-words shadow-md">
                                 {data.content}
                             </div>
                         </div>
