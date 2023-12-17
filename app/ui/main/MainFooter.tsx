3843ea71e5611963ffd1adb5ff2ff7163e06f75c
Hyungjun <dlgudwns1207@gmail.com>
2023-12-17 16:59:17 +0900

    Feat: Add notion link on MainFooter

--- app/ui/main/MainFooter.tsx
+++ app/ui/main/MainFooter.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 const MainFooter = () => {
     return (
         <div className="flex flex-col justify-center items-center text-[#FFFFFF] p-8 gap-4 bg-[#7A34F2] w-screen h-full mt-44 font-[400]">
             <p>Team CTRL Ⓒ All Rights Reserved</p>
-            <p>About us</p>
+            <Link href="https://elemental-chimpanzee-ca2.notion.site/CTRL-dbd4fea787884b5381fee8d8e51c331d">
+                <p className="underline underline-offset-2">About us</p>
+            </Link>
             <p>개발 박종헌 | 디자인 서정민 | 개발 이형준, 신문기</p>
         </div>
     );
