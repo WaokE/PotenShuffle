d166d89d4a852eb720ad0ba81d01fe469e7f590b
Hyungjun <dlgudwns1207@gmail.com>
2023-12-17 17:15:40 +0900

    Feat: Add static image in Team componet

--- app/ui/main/Teams/Teams.tsx
+++ app/ui/main/Teams/Teams.tsx
@@ -1,5 +1,14 @@
 const Teams = () => {
-    return <div>Teams</div>;
+    return (
+        <div
+            className="w-[1200px] h-[890px]"
+            style={{
+                backgroundImage: "url('/Card.png')",
+                backgroundSize: "contain",
+                backgroundPosition: "center",
+            }}
+        ></div>
+    );
 };
 
 export default Teams;
