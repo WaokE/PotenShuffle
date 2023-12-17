415a20ec37986d598bf72b802f02ad0e0e2ce10d
Hyungjun <dlgudwns1207@gmail.com>
2023-12-17 12:30:24 +0900

    Feat: Show received offers

--- app/ui/main/Dashboard/ReceivedOffers.tsx
+++ app/ui/main/Dashboard/ReceivedOffers.tsx
@@ -1,8 +1,66 @@
+import Image from "next/image";
+
+import { useEffect } from "react";
+
+import { useCurrentUserStore } from "@/store/currentUserStore";
+import { useDashboardStore } from "@/store/dashboardStore";
+
+import { fetchReceivedOffers } from "@/app/api/teamOffer";
+
 const ReceivedOffers = () => {
+    const token = useCurrentUserStore((state) => state.user.token);
+    const { receivedOffers, setReceivedOffers } = useDashboardStore();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const receivedOffers = await fetchReceivedOffers(token);
+            console.log(receivedOffers.data.items);
+            setReceivedOffers(receivedOffers.data.items);
+        };
+
+        fetchData();
+    }, []);
+
+    const getOccupationIcon = (occupation: string) => {
+        switch (occupation) {
+            case "DESIGNER":
+                return <Image src="/_Type=5.svg" alt="icon" width="30" height="30" />;
+            case "WEB_ENGINEER":
+                return <Image src="/_Type=2.svg" alt="icon" width="30" height="30" />;
+            case "SERVER_ENGINEER":
+                return <Image src="/_Type=4.svg" alt="icon" width="30" height="30" />;
+            case "PRODUCT_MANAGER":
+                return <Image src="/_Type=1.svg" alt="icon" width="30" height="30" />;
+            default:
+                return null;
+        }
+    };
+
     return (
-        <div className="flex justify-center h-1/2-screen w-full bg-[url('/noResult.svg')] bg-center bg-cover">
-            <p className="select-none text-gray-400">받은 제안이 없습니다.</p>
-        </div>
+        <>
+            {receivedOffers.length === 0 ? (
+                <div className="flex justify-center h-1/2-screen w-full bg-[url('/noResult.svg')] bg-center bg-cover">
+                    <p className="select-none text-gray-400">받은 제안이 없습니다.</p>
+                </div>
+            ) : (
+                <div className="flex flex-col gap-1 h-1/2-screen w-full">
+                    {receivedOffers.map((offer) => (
+                        <div
+                            key={offer.id}
+                            className={`flex border h-1/6 bg-white rounded-lg shadow select-none ${
+                                offer.status === "DECLINED" ? "opacity-50" : ""
+                            }`}
+                        >
+                            {getOccupationIcon(offer.sentUser.card.occupation)}
+                            <div className="flex flex-col gap-1">
+                                <p>{offer.sentUser.name}님에게 제안을 보냈습니다.</p>
+                                <p>{offer.receivedAt}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </>
     );
 };
 
--- store/dashboardStore.ts
+++ store/dashboardStore.ts
@@ -1,13 +1,35 @@
 import { create } from "zustand";
 
+import { userInfo } from "@/type/user";
+
 export type NavigationElement = "myIntroduce" | "receivedOffer" | "sentOffer";
+
+export type ReceivedOffer = {
+    id: number;
+    receivedAt: string;
+    sentUser: {
+        id: number;
+        name: string;
+        card: userInfo;
+    };
+    sentUserTeamMembers: {
+        id: number;
+        name: string;
+        card: userInfo;
+    }[];
+    status: "NONE" | "PENDING" | "ACCEPTED" | "DECLINED";
+};
 
 type DashboardStore = {
     selectedNavigation: NavigationElement;
     setSelectedNavigation: (navigation: NavigationElement) => void;
+    receivedOffers: ReceivedOffer[];
+    setReceivedOffers: (receivedOffers: ReceivedOffer[]) => void;
 };
 
 export const useDashboardStore = create<DashboardStore>((set) => ({
     selectedNavigation: "myIntroduce",
     setSelectedNavigation: (navigation) => set({ selectedNavigation: navigation }),
+    receivedOffers: [],
+    setReceivedOffers: (receivedOffers) => set({ receivedOffers }),
 }));
